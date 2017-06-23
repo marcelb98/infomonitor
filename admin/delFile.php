72f8c2a84b532dba4e39d0c003408492282fb2b5
Marcel Beyer <git_mbeyer@it-maker.eu>
2017-06-23 12:53:41 +0200

    datei löschen und dabei aus aktiver liste entfernen

--- admin/delFile.php
+++ admin/delFile.php
@@ -10,6 +10,24 @@ if($r != 'y'){
 	echo '<a href="delFile.php?r=y&n='.$n.'">JA</a> <a href="index.php">NEIN</a><br>';
 	echo '</body></html>';
 }else{
+	//Prüfen, ob gerade aktiviert
+	@$links = file_get_contents("../data/links.json");
+	$links = json_decode($links);
+	if( @in_array($name, $links) ){
+		$i = array_search($name, $links);
+		$links = array_splice($links, $i, 1);
+	}
+	$links = json_encode($links);
+	file_put_contents("../data/links.json", $links);
+	@$rechts = file_get_contents("../data/rechts.json");
+	$rechts = json_decode($rechts);
+	if( @in_array($name, $rechts) ){
+		$i = array_search($name, $rechts);
+		$rechts = array_splice($rechts, $i, 1);
+	}
+	$rechts = json_encode($rechts);
+	file_put_contents("../data/rechts.json", $rechts);
+	
 	$result = unlink("../files/".$name);
 	if($result){
 		header("Location: index.php");
